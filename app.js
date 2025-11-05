343c7d0af7c76254daeb524c3dd6d8603bc680e2
Rahil Qayoom <217341844+RahilQayoom@users.noreply.github.com>
2025-11-05 10:03:24 +0000

    Home Page of Tea Station

---
+++ app.js
@@ -0,0 +1,18 @@
+// DATE SETUP
+const date = (document.getElementById("date").innerHTML =
+  new Date().getFullYear());
+// SETUP NAV
+const navBtn = document.getElementById("nav-btn");
+const navbar = document.getElementById("navbar");
+const navClose = document.getElementById("nav-close");
+
+// show nav
+navBtn.addEventListener("click", () => {
+  navbar.classList.add("showNav");
+});
+
+// close nav
+
+navClose.addEventListener("click", () => {
+  navbar.classList.remove("showNav");
+});
